217c26b939a4a203796f77e88c60684beecaca46
Owen Hong <owenhong04@gmail.com>
2026-02-27 16:23:16 +0800

    Added the UserIcon and ExportButton

--- src/main/java/CourseRegisterUI/CourseController.java
+++ src/main/java/CourseRegisterUI/CourseController.java
@@ -1,10 +1,14 @@
 package CourseRegisterUI;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class CourseController {
+    @FXML private HBox userIcon;
+    @FXML private HBox exportButton;
     @FXML private VBox courseListPane;
     @FXML private BorderPane schedulePane;
     @FXML private MenuBar menuBar;
@@ -14,5 +18,7 @@ public class CourseController {
         courseListPane.getChildren().setAll(ComponentLoader.loadSidePanel());
         schedulePane.setCenter(ComponentLoader.loadWeeklyCalendar());
         menuBar.getMenus().addAll(ComponentLoader.loadMenuBar().getMenus());
+        userIcon.getChildren().setAll(ComponentLoader.loadUserIcon());
+        exportButton.getChildren().setAll(ComponentLoader.loadExportButton());
     }
 }
