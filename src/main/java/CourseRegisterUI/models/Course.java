4c49a0c9ad90aac0124d28da3df2d8f2f70d2480
Owen Hong <owenhong04@gmail.com>
2026-03-25 22:45:46 +0800

    Added College enum and all fields for course as primitive types

--- src/main/java/CourseRegisterUI/models/Course.java
+++ src/main/java/CourseRegisterUI/models/Course.java
@@ -1,5 +1,28 @@
 package CourseRegisterUI.models;
 
+import java.util.Date;
+
 public record Course(
         // TODO fill in later
+        String academic_unit,
+        String subject,
+        String course_code,
+        String title,
+
+        String crn,
+        String section,
+        Integer credit,
+        String campus,
+        Boolean web_enabled,
+        String level,
+        Integer availability,
+        Integer cap,
+        Boolean waitlist_available,
+        Date start_date,
+        Date end_date,
+        String day,
+        String building,
+        String room,
+        Teacher instructor,
+        String medium
 ) {}
