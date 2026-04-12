4d88c3e5971c7c247ee9fe0299daff3b419f2b07
Owen Hong <owenhong04@gmail.com>
2026-04-12 16:30:43 +0800

    Added functionality for filtering for the side table

--- src/main/java/CourseRegisterUI/controllers/SidePanelController.java
+++ src/main/java/CourseRegisterUI/controllers/SidePanelController.java
@@ -3,23 +3,31 @@ package CourseRegisterUI.controllers;
 import CourseRegisterUI.AppContext;
 import CourseRegisterUI.ComponentLoader;
 import CourseRegisterUI.ContextAware;
+import CourseRegisterUI.models.College;
+import CourseRegisterUI.models.Course;
 import CourseRegisterUI.models.CourseRow;
 import CourseRegisterUI.models.Root;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Window;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 
 public class SidePanelController implements ContextAware {
     @FXML
     private Button addCourseBtn;
     @FXML
-    private VBox courseListPane;
+    private ScrollPane courseListPane;
     @FXML
     private TableView<CourseRow> courseTable;
     @FXML
@@ -55,14 +63,44 @@ public class SidePanelController implements ContextAware {
     @FXML
     private TableColumn<CourseRow, String> meetingColumn;
 
+    @FXML private ComboBox<String> academicUnitComboBox;
+    @FXML private TextField academicUnitTextField;
+    @FXML private TextField search_by_text_field;
+    @FXML private CheckBox creditCheckBox;
+    @FXML private TextField creditTextField;
+    @FXML private RadioButton exactCreditButton;
+    @FXML private CheckBox programCheckBox;
+    @FXML private ComboBox<College> programComboBox;
+    @FXML private CheckBox mediumCheckBox;
+    @FXML private ComboBox<String> mediumComboBox;
 
     // this represents ALL of that pane, not just the course list box
     private AppContext context;
     private CourseController mainController;
+    private FilteredList<CourseRow> filteredList;
+
+    @Override
+    public void setAppContext(AppContext appContext) {
+        this.context = appContext;
+        filteredList = new FilteredList<>(context.getSelectedCourseRows(), p -> true);
+        courseTable.setItems(filteredList);
+
+        SortedList<CourseRow> sortedData = new SortedList<>(filteredList);
+        sortedData.comparatorProperty().bind(courseTable.comparatorProperty());
+        courseTable.setItems(sortedData);
+
+        attachFilters();
+        context.getSelectedCourseRows().addListener((ListChangeListener<CourseRow>) change -> {
+            while (change.next()) {
+                loadFilters();
+            }
+        });
+    }
 
 
     @FXML
     public void initialize() {
+        courseListPane.setFitToWidth(true);
         addCourseBtn.getStyleClass().add("btn-submit");
         courseTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         subjectColumn.setCellValueFactory(cell -> cell.getValue().getProperty("subject"));
@@ -91,12 +129,6 @@ public class SidePanelController implements ContextAware {
         this.mainController.setAppContext(this.context); // TODO double check this later
     }
 
-    @Override
-    public void setAppContext(AppContext appContext) {
-        this.context = appContext;
-        courseTable.setItems(appContext.getSelectedCourseRows());
-    }
-
     @FXML
     private void handleAddCourseDialog() {
         WindowController.showModal(
@@ -105,5 +137,110 @@ public class SidePanelController implements ContextAware {
                 "Add New Course",
                 context
         );
+    }
+
+    private void applyFilters() {
+        filteredList.setPredicate(row -> {
+            Course c = row.getCourse();
+
+            String courseTitle = search_by_text_field.getText();
+            if (courseTitle != null && !courseTitle.isBlank()) {
+                String s = courseTitle.toLowerCase();
+                if (!c.title().toLowerCase().contains(s) && !c.course_code().toLowerCase().contains(s)) {
+                    return false;
+                }
+            }
+
+            String nameText = academicUnitTextField.getText();
+            String nameChoice = academicUnitComboBox.getValue();
+
+            if (nameText != null && !nameText.isBlank()) {
+                String s = nameText.toLowerCase();
+                if (!c.academic_unit().toLowerCase().contains(s) && !c.course_code().toLowerCase().contains(s)) {
+                    return false;
+                }
+            }
+
+            if (nameChoice != null && !nameChoice.isBlank()) {
+                if (!c.academic_unit().equalsIgnoreCase(nameChoice)) {
+                    return false;
+                }
+            }
+
+            if (creditCheckBox.isSelected()) {
+                if (creditTextField.getText() == null || creditTextField.getText().isBlank()) {
+                    return false;
+                }
+
+                try {
+                    int credit = Integer.parseInt(creditTextField.getText().trim());
+                    if (exactCreditButton.isSelected()) {
+                        if (!c.credit().equals(credit)) return false;
+                    } else {
+                        if (c.credit() < credit) return false;
+                    }
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+
+            if (programCheckBox.isSelected()) {
+                College selectedCollege = programComboBox.getValue();
+                if (selectedCollege != null) {
+                    if (!c.college().equals(selectedCollege)) {
+                        return false;
+                    }
+                }
+            }
+
+            if (mediumCheckBox.isSelected()) {
+                String medium = mediumComboBox.getValue();
+                if (medium != null && !medium.isBlank()) {
+                    if (!c.medium().equalsIgnoreCase(medium)) return false;
+                }
+            }
+
+            return true;
+        });
+    }
+
+    private void attachFilters() {
+        loadFilters();
+
+        search_by_text_field.textProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+        academicUnitComboBox.valueProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+        academicUnitTextField.textProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+
+        creditCheckBox.selectedProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+        creditTextField.textProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+        exactCreditButton.selectedProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+
+        programCheckBox.selectedProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+        programComboBox.valueProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+
+        mediumCheckBox.selectedProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+        mediumComboBox.valueProperty().addListener((obs, oldVal, newVal) -> applyFilters());
+    }
+
+    public void loadFilters() {
+        List<String> course_titles = new ArrayList<>(context.getSelectedCourseRows().stream()
+                .map(courseRow -> courseRow.getCourse().academic_unit())
+                .distinct()
+                .sorted()
+                .toList());
+        course_titles.addFirst(null);
+
+        academicUnitComboBox.setItems(FXCollections.observableArrayList(course_titles));
+
+        programComboBox.setItems(FXCollections.observableArrayList(
+                Stream.concat(Stream.of((College) null), Arrays.stream(College.values()))
+                        .toList()
+        ));
+
+        mediumComboBox.setItems(FXCollections.observableArrayList(
+                null,
+                "English",
+                "Chinese"
+        ));
     }
 }
